5de71e6e342d946e81f9ebd0d99f7aced6427ec9
S-vz <s.vrzina@student.vu.nl>
2024-02-29 16:02:02 +0100

    JWT authentication for registration and login API

--- api/views.py
+++ api/views.py
@@ -4,49 +4,25 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 import os, re
 
-import rest_framework_simplejwt.serializers
+from rest_framework import generics, serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.validators import UniqueValidator
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenSerializer, RegisterSerializer
+
 
 __ROOT__ = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def test(request):
     return JsonResponse({"data": "test"})
 
+# Acts as the "login" API, provide username and password to get the access and refresh token
+class CustomTokenView(TokenObtainPairView):
+    serializer_class = CustomTokenSerializer
+
+# The register API, requires the fields outlined in the RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
-
-class CustomTokenSerializer(serializers.TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token['username'] = user.username
-        token['email'] = user.email
-        return token
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password', "first_name", "last_name")
-
-    def validate(self, attrs):
-        return attrs
-
-    def create(self, data):
-        user = User.objects.create(
-            username=data['username'],
-            email=data['email'],
-            first_name=data['first_name'],
-            last_name=data['last_name']
-        )
-
-        user.set_password(data['password'])
-        user.save()
-        return user
