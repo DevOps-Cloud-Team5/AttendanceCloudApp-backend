09db2ad12cbb2723c235b38759ec63a410766def
S-vz <s.vrzina@student.vu.nl>
2024-03-24 23:05:15 +0100

    remove open permissions, now roles are enforced

--- api/permissions.py
+++ api/permissions.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 class IsRole(permissions.BasePermission):
     role_list = []
     def has_permission(self, request, _=None):
-        return True # For debugging, full open permission is given
+        # return True # For debugging, full open permission is given
         return hasattr(request.user, "role") and request.user.role in self.role_list
 
 class IsStudent(IsRole): role_list = ["admin", "teacher", "student"]
