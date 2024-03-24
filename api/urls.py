7e821552406da763ff2da17654567a0a682aa9ef
Max <maxmb15@gmail.com>
2024-03-24 16:06:53 +0100

    test change to deploy db

--- api/urls.py
+++ api/urls.py
@@ -30,7 +30,6 @@ from .views import (
                     SetStudentAttView,
                     SetTeacherAttView,
                     test,
-                    test2,
                     MailTestView,
                     UnsetStudentAttView,
                     genAdmin, 
@@ -53,7 +52,6 @@ from .views import (
 
 urlpatterns = [
     path('test', test),
-    path('test2', test2),
     path('send_welcome_email', MailTestView.as_view(), name='send_welcome_email'),
     path('genadmin', genAdmin),
     
